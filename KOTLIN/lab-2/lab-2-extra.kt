a1ee72329ed1b826b1279bf596d3e5f15f10fe1d
Zsigmond Attila <zsigmond.attila@student.ms.sapientia.ro>
2024-10-13 18:46:25 +0300

    Solved lab2 extra

--- KOTLIN/lab-2/lab-2-extra.kt
+++ KOTLIN/lab-2/lab-2-extra.kt
@@ -1,24 +1,98 @@
 import kotlin.random.Random
 
 class TextGenerator {
-    private val prefixMap = mutableMapOf<Pair<String, String>, MutableList<String>>()
+    val wordPairs = mutableMapOf<Pair<String, String>, MutableList<String>>()
+    var firstTwoWords: Pair<String, String>? = null
 
-    fun learnWords(text: String) {
-
+    fun learnWords(input: String) {
+        val words = input.split(" ")
+        
+        if (words.size < 3) return
+        
+        firstTwoWords = Pair(words[0], words[1])
+        
+        for (i in 0 until words.size - 2) {
+            val prefix = Pair(words[i], words[i + 1])
+            val postfix = words[i + 2]
+            wordPairs.getOrPut(prefix) { mutableListOf() }.add(postfix)
+        }
     }
 
     fun generateText(): String {
-        return ""
+        val result = StringBuilder()
+        var currentPrefix = firstTwoWords
+        
+        if (currentPrefix != null) {
+            result.append(currentPrefix.first).append(" ").append(currentPrefix.second)
+        }
+
+        while (currentPrefix != null) {
+            val postfixes = wordPairs[currentPrefix]
+
+            if (postfixes.isNullOrEmpty()) break
+
+            val nextWord = postfixes[Random.nextInt(postfixes.size)]
+            result.append(" ").append(nextWord)
+
+            currentPrefix = Pair(currentPrefix.second, nextWord)
+        }
+
+        return result.toString()
     }
 }
 
 fun main() {
-    val text = "Now is not the time for sleep, now is the time for party!"
-    val textGenerator = TextGenerator()
-
-    textGenerator.learnWords(text)
-
-    val generatedText = textGenerator.generateText()
+    testLearnWords()
+    testGenerateText_correctFirstTwoWords()
+    testGenerateText_generatesTextBasedOnLearnedPairs()
+    
+    val input = "Now is not the time for sleep, now is the time for party!"
+    val generator = TextGenerator()
+    generator.learnWords(input)
+    val generatedText = generator.generateText()
     println(generatedText)
 }
 
+
+fun testLearnWords() {
+    val input = "Now is the time for sleep, now is the time for party!"
+    val generator = TextGenerator()
+    generator.learnWords(input)
+
+    val expectedPostfixesForTimeFor = listOf("sleep,", "party!")
+    val actualPostfixesForTimeFor = generator.wordPairs[Pair("time", "for")]
+
+    if (actualPostfixesForTimeFor != null && actualPostfixesForTimeFor.containsAll(expectedPostfixesForTimeFor)) {
+        println("testLearnWords PASSED")
+    } else {
+        println("testLearnWords FAILED")
+    }
+}
+
+fun testGenerateText_correctFirstTwoWords() {
+    val input = "Now is not the time for sleep, now is the time for party!"
+    val generator = TextGenerator()
+    generator.learnWords(input)
+
+    val generatedText = generator.generateText()
+    if (generatedText.startsWith("Now is")) {
+        println("testGenerateText_correctFirstTwoWords PASSED")
+    } else {
+        println("testGenerateText_correctFirstTwoWords FAILED")
+    }
+}
+
+fun testGenerateText_generatesTextBasedOnLearnedPairs() {
+    val input = "Now is not the time for sleep, now is the time for party!"
+    val generator = TextGenerator()
+    generator.learnWords(input)
+
+    val generatedText = generator.generateText()
+    val words = generatedText.split(" ")
+
+    if (words.size >= 6 && words[0] == "Now" && words[1] == "is") {
+        println("testGenerateText_generatesTextBasedOnLearnedPairs PASSED")
+    } else {
+        println("testGenerateText_generatesTextBasedOnLearnedPairs FAILED")
+    }
+}
