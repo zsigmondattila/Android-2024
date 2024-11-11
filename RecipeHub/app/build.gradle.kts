4fc45b970503e538a8447fb7323a6853d0df6318
Zsigmond Attila <zsigmond.attila@student.ms.sapientia.ro>
2024-11-11 20:46:44 +0200

    Changed color palette

--- RecipeHub/app/build.gradle.kts
+++ RecipeHub/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation("com.google.code.gson:gson:2.8.8")
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- RecipeHub/app/src/main/java/com/example/recipehub/activity/MainActivity.kt
+++ RecipeHub/app/src/main/java/com/example/recipehub/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.recipehub
+package com.example.recipehub.activity
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.recipehub.R
 import com.example.recipehub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
