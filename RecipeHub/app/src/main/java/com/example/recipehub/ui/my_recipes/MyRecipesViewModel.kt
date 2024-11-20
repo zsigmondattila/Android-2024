b428d4a09a8381a159a6d35091d4479caccaff5c
Zsigmond Attila <zsigmond.attila@student.ms.sapientia.ro>
2024-11-20 17:58:47 +0200

    Added button actions + my recipes adapter and viewmodel

---
+++ RecipeHub/app/src/main/java/com/example/recipehub/ui/my_recipes/MyRecipesViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.recipehub.ui.my_recipes
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.recipehub.domain.model.RecipeModel
+import com.example.recipehub.repository.recipe.RecipeRepository
+
+class MyRecipesViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val recipeRepository = RecipeRepository(application)
+
+    private val _recipes = MutableLiveData<List<RecipeModel>>()
+    val recipes: LiveData<List<RecipeModel>> get() = _recipes
+
+    fun fetchRecipes() {
+        val recipeList = recipeRepository.getAllRecipes()
+
+        Log.d("RecipeListViewModel", "Fetched Recipes: $recipeList")
+
+        _recipes.value = recipeList
+    }
+}
--- RecipeHub/app/src/main/java/com/example/recipehub/ui/profile/ProfileFragment.kt
+++ RecipeHub/app/src/main/java/com/example/recipehub/ui/profile/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.recipehub.R
 
 class ProfileFragment : Fragment() {
@@ -15,5 +17,33 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btnPersonalInfo).setOnClickListener {
+            navigateToPersonalInfo()
+        }
+
+        view.findViewById<Button>(R.id.btnMyRecipes).setOnClickListener {
+            navigateToMyRecipes()
+        }
+
+        view.findViewById<Button>(R.id.btnSettings).setOnClickListener {
+            navigateToSettings()
+        }
+    }
+
+    private fun navigateToPersonalInfo() {
+        findNavController().navigate(R.id.action_navigation_profile_to_navigation_personal_info)
+    }
+
+    private fun navigateToSettings() {
+        findNavController().navigate(R.id.action_navigation_profile_to_navigation_settings)
+    }
+
+    private fun navigateToMyRecipes() {
+        findNavController().navigate(R.id.action_navigation_profile_to_navigation_my_recipes)
     }
 }
